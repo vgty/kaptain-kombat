1eaf10d6df16e7e81ca11e98d20d262fdc72d921
vgty <victor.goutay@gmail.com>
2018-09-19 12:17:30 +0200

    13 Solving CSS issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -78,6 +78,14 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  #Assets loading
+  
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
+
+
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
