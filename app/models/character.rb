5ff5bfb9a324ccce1de2b1992992fd4db3dece5e
vgty <victor.goutay@gmail.com>
2018-09-21 14:52:04 +0200

    21 Adding validation to names

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,5 @@
 class Character < ApplicationRecord
+  
   has_many :fights
   has_one :overall_character_stat, dependent: :destroy
   has_many :fight_character_stats
@@ -8,4 +9,6 @@ class Character < ApplicationRecord
   validates_presence_of :name, :life_points, :attack_points
   validates_inclusion_of :life_points, in: 8..15
   validates_inclusion_of :attack_points, in: 5..10
+  validates :name, uniqueness:   true,
+
 end
--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,4 +1,5 @@
 class Weapon < ApplicationRecord
+  validates :name, uniqueness:   true,
   validates_presence_of :name, :attack_force
   validates_inclusion_of :attack_force, in: 10..15
 end
